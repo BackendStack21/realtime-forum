de38b894a9a34f594078af805553761011c8e711
Rolando Santamaria Maso <kyberneees@gmail.com>
2023-11-25 00:37:16 +0100

    adding backend-clock-stream demo

---
+++ demos/node/backend-clock-stream/backend.js
@@ -0,0 +1,44 @@
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
+
+const { getAuthToken } = require('../utils')
+
+const {
+  APP_ID,
+  ADMIN_SIGNING_KEY, // For HS* signing algorithms, value should be "Admin Clients/Verification Key"
+  ALGORITHM,
+  CLUSTER_HOSTNAME
+} = require('../config') // Import configuration
+
+const URL = `https://${CLUSTER_HOSTNAME}/api/topics/${APP_ID}/publish`
+const TOPIC = 'backend-clock'
+
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
+setInterval(() => {
+  const AUTH_TOKEN = getAuthToken(ADMIN_SIGNING_KEY, 5, {
+    roles: ['Publisher'],
+    allowedTopics: ['*']
+  }, ALGORITHM, 'app-publisher')
+
+  options.headers.Authorization = `Bearer ${AUTH_TOKEN}`
+  options.body = JSON.stringify({
+    topic: TOPIC,
+    message: new Date().toISOString(),
+    compress: false
+  })
+
+  const startTime = new Date().getTime()
+  fetch(URL, options).then(async response => {
+    console.log({
+      statusText: response.statusText,
+      status: response.status
+    })
+
+    console.log('Publishing latency (HTTP endpoint): ' + (new Date().getTime() - startTime) + 'ms')
+  })
+}, 1000)
---
+++ demos/node/backend-clock-stream/client.js
@@ -0,0 +1,35 @@
+const W3CWebSocket = require('websocket').w3cwebsocket
+const { getAuthToken } = require('../utils')
+
+const {
+  APP_ID,
+  WEBSOCKET_CLIENTS_SIGNING_KEY, // For HS* signing algorithms, value should be "WebSocket Clients/Verification Key"
+  ALGORITHM,
+  CLUSTER_HOSTNAME
+} = require('../config') // Import configuration
+
+const ACCESS_TOKEN = getAuthToken(WEBSOCKET_CLIENTS_SIGNING_KEY, 5, {}, ALGORITHM)
+
+const ws = new W3CWebSocket(`wss://${CLUSTER_HOSTNAME}/apps/${APP_ID}?access_token=${ACCESS_TOKEN}`)
+ws.onmessage = (event) => {
+  const { topic, messageType, data } = JSON.parse(event.data)
+  if (topic === 'main' && messageType === 'welcome') {
+    console.log('> Connected!')
+
+    ws.send(JSON.stringify({
+      type: 'subscribe',
+      data: {
+        topic: 'backend-clock'
+      }
+    }))
+  } else if (topic === 'backend-clock' && messageType === 'broadcast') {
+    console.log('> Server time: ' + data)
+    console.log('- Propagation delay (End to End): ' + (new Date().getTime() - new Date(data).getTime()) + 'ms')
+  }
+}
+ws.onclose = ({ code, reason }) => {
+  console.log('Connection closed: ', {
+    code,
+    reason
+  })
+}
