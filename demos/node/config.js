d7c8d403bc98883342ae13dd6d12c1cd79e20dd1
Rolando Santamaria Maso <4096860+jkyberneees@users.noreply.github.com>
2023-11-25 14:01:36 +0100

    create config.js
    adding config.js template file

---
+++ demos/node/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  APP_ID: 'YOUR APPLICATION ID HERE...',
+  WEBSOCKET_CLIENTS_SIGNING_KEY: 'SIGNING KEY HERE...', // For HS* signing algorithms, value should be "WebSocket Clients/Verification Key"
+  ADMIN_SIGNING_KEY: 'SIGNING KEY HERE...',             // For HS* signing algorithms, value should be "Admin Clients/Verification Key"
+  
+  ALGORITHM: 'HS256',
+  CLUSTER_HOSTNAME: 'genesis.r7.21no.de',
+  API_GATEWAY_HOSTNAME: 'api-gateway.r7.21no.de'
+}
