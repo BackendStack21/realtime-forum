04c8658d2e0c6af17ef99fe36fad97b0a6fec36b
Rolando Santamaria Maso <kyberneees@gmail.com>
2024-07-14 17:29:46 +0200

    feat: fixing typo

--- demos/node/config.js
+++ demos/node/config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  APP_ID: 'YOUR APPLICATION ID HERE...',
-  WEBSOCKET_CLIENTS_SIGNING_KEY: 'SIGNING KEY HERE...', // For HS* signing algorithms, value should be "WebSocket Clients/Verification Key"
-  ADMIN_SIGNING_KEY: 'SIGNING KEY HERE...',             // For HS* signing algorithms, value should be "Admin Clients/Verification Key"
-  
-  ALGORITHM: 'HS256',
-  CLUSTER_HOSTNAME: 'genesis.r7.21no.de',
-  API_GATEWAY_HOSTNAME: 'api-gateway.r7.21no.de'
+  "APP_ID": "77229305dcc0c3c8b2aeff32d92122be",
+  "WEBSOCKET_CLIENTS_SIGNING_KEY": "TkAmcIxp9D0unh/q9vdAFdEikclqPPFrcIPsLA1HwcQ=",
+  "ADMIN_SIGNING_KEY": "TkAmcIxp9D0unh/q9vdAFdEikclqPPFrcIPsLA1HwcQ=",
+  "ALGORITHM": "HS256",
+  "CLUSTER_HOSTNAME": "genesis.r7.21no.de",
+  "API_GATEWAY_HOSTNAME": "api-gateway.r7.21no.de"
 }
